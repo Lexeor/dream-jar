aefdf848c02504e888e71472f7c2647e20b9b879
Lexeor <lexeor.ross@gmail.com>
2025-03-10 20:00:27 +0100

    remove user auth for telegram

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import {init} from "./init.ts";
-import { isTMA } from '@telegram-apps/bridge';
-
-isTMA() && init();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
